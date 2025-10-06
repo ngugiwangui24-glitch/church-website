d5e12b519e11014e7f9f31d318efdc2bc37f7afd
ngugiJohn02 <ngugijohn728@gmail.com>
2025-10-06 03:59:26 +0300

    Added Event and Sermon models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User  # For associating events/sermons with users
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Sermon(models.Model):
+    title = models.CharField(max_length=200)
+    preacher = models.CharField(max_length=100)
+    content = models.TextField()  # Can expand to FileField for audio/video later
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
